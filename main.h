4ded47d0d0892bcb918b76bd1d1aac29ddaef7e3
JeremMac <macal.jerem@gmail.com>
2023-08-23 03:01:31 -0700

    file with with prototype

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef __main__h__
+#define  __main__h__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void execute(char **args);
+void split_string(char *str);
+
+#endif
